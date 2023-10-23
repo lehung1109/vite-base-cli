c5dcb02d0d90ffadffcf2273c78451022573c251
Hung Le <hung.le@optimizely.com>
2023-10-23 22:03:26 +0700

    Update base cli

--- cli/install-template.ts
+++ cli/install-template.ts
@@ -1,10 +1,16 @@
 import fs from 'fs/promises';
-import path from 'path';
+import path, { join } from 'path';
 import { fileURLToPath } from 'url';
-import { copy } from '../helpers/copy.js';
 import os from 'os';
 import chalk from 'chalk'
 import { install } from '../helpers/install.js';
+import { tmpdir } from 'os';
+import { promisify } from 'util';
+import { Stream } from 'stream';
+import got from 'got';
+import tar from 'tar'
+import { createWriteStream } from 'fs';
+import pRetry from 'p-retry';
 
 export const filename = fileURLToPath(import.meta.url);
 export const dirname = path.dirname(filename);
@@ -14,15 +20,38 @@ interface Props {
   root: string;
 }
 
+const pipeline = promisify(Stream.pipeline)
+
 const { cyan } = chalk;
+
+const downloadTar = async (url: string) => {
+  const tempFile = join(tmpdir(), `vite.temp-${Date.now()}`);
+
+  await pipeline(got.stream(url), createWriteStream(tempFile));
+
+  return tempFile;
+}
+
+const downloadAndExtract = async (root: string) => {
+  const tempFile = await downloadTar(
+    'http://codeload.github.com/lehung1109/vite-base/tar.gz/main'
+  );
+
+  await tar.x({
+    file: tempFile,
+    cwd: root,
+    strip: 1,
+  });
+
+  await fs.unlink(tempFile)
+}
 
 const installTemplate = async (model: Props) => {
   const { appName, root } = model;
   console.log('\nInitializing project');
-  const copySource = ['**'];
 
-  await copy(copySource, root, {
-    cwd: path.join(dirname, '..')
+  await pRetry(async () => downloadAndExtract(root), {
+    retries: 3,
   });
 
   const packageJson = JSON.parse(await fs.readFile('package.json', 'utf8'));
@@ -30,7 +59,7 @@ const installTemplate = async (model: Props) => {
   packageJson.name = appName;
   packageJson.description = '';
   packageJson.version = '0.1.0';
-  packageJson.bin = './bin/cli.js';
+  packageJson.bin = '';
 
   const devDeps = Object.keys(packageJson.devDependencies).length;
 
